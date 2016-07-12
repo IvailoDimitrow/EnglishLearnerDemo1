a4d17de620888146709f5485ae98c538f08e2020
IvailoDimitrow <ividim@mail.bg>
2016-07-12 10:17:45 +0300

    Adding Test and Transformations exercises

--- app/src/main/java/com/example/acer/englishlearner/GrammarCausativeFormActivity3.java
+++ app/src/main/java/com/example/acer/englishlearner/GrammarCausativeFormActivity3.java
@@ -1,84 +1,68 @@
 package com.example.acer.englishlearner;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class GrammarCausativeFormActivity3 extends AppCompatActivity {
     int score;
     TextView text;
+    Button btn,btn2,btn3,btn4,btn5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frame_causativeform_three);
         score = 0;
         text = (TextView)findViewById(R.id.score_causativeform);
+        btn = (RadioButton) findViewById(R.id.radio_pirates1);
+        btn2 = (RadioButton) findViewById(R.id.radio_pirates2);
+        btn3 = (RadioButton) findViewById(R.id.radio_ninjas3);
+        btn4 = (RadioButton) findViewById(R.id.radio_ninjas4);
+        btn5 = (RadioButton) findViewById(R.id.radio_pirates5);
     }
     public void onRadioButtonClicked(View view) {
         boolean checked = ((RadioButton) view).isChecked();
         switch (view.getId()) {
             case R.id.radio_pirates1:
                 if (checked) {
-                    score++;
-                    text.setText(score);
-                    break;
+                    btn.setBackgroundColor(Color.GREEN);
+                } else {
+                    btn.setBackgroundColor(Color.GREEN);
                 }
-            case R.id.radio_ninjas1:
-                if (checked) {
-
-                    break;
-
-                }
+                break;
             case R.id.radio_pirates2:
                 if (checked) {
-                    score++;
-                    text.setText(score);
-                    break;
-                }
-            case R.id.radio_ninjas2:
-                if (checked) {
-
-                    break;
+                    btn2.setBackgroundColor(Color.GREEN);
+                } else {
+                    btn2.setBackgroundColor(Color.GREEN);
 
                 }
-            case R.id.radio_pirates3:
-                if (checked) {
-
-                    break;
-                }
+                break;
             case R.id.radio_ninjas3:
                 if (checked) {
-                    score++;
-                    text.setText(score);
-                    break;
-
+                    btn3.setBackgroundColor(Color.GREEN);
+                } else {
+                    btn3.setBackgroundColor(Color.GREEN);
                 }
-            case R.id.radio_pirates4:
-                if (checked) {
-
-                    break;
-                }
+                break;
             case R.id.radio_ninjas4:
                 if (checked) {
-                    score++;
-                    text.setText(score);
-                    break;
-
+                    btn4.setBackgroundColor(Color.GREEN);
+                } else {
+                    btn4.setBackgroundColor(Color.GREEN);
                 }
+                break;
             case R.id.radio_pirates5:
                 if (checked) {
-                    score++;
-                    text.setText(score);
-                    break;
+                    btn5.setBackgroundColor(Color.GREEN);
+                } else {
+                    btn5.setBackgroundColor(Color.GREEN);
                 }
-            case R.id.radio_ninjas5:
-                if (checked) {
-
-                    break;
-
-                }
+                break;
         }
     }
 }
--- app/src/main/java/com/example/acer/englishlearner/PassiveTransformations.java
+++ app/src/main/java/com/example/acer/englishlearner/PassiveTransformations.java
@@ -1,13 +1,59 @@
 package com.example.acer.englishlearner;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PassiveTransformations extends AppCompatActivity {
-
+    EditText text,text2,text3,text4;
+    String sentence,sentence2,sentence3,sentence4;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.passive_grammar_transformations);
+
+        text = (EditText)findViewById(R.id.edt1);
+        text2 = (EditText)findViewById(R.id.edt2);
+        text3 = (EditText)findViewById(R.id.edt3);
+        text4 = (EditText)findViewById(R.id.edt4);
+        btn = (Button)findViewById(R.id.btn_check);
+
+        sentence = "The work has been being done by John.";
+        sentence2 = "The work will be finished by 5:00 PM.";
+        sentence3 = "The pies would always be made by my mother.";
+        sentence4 = "The bills used to be paid by Jerry.";
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(text.equals(sentence)){
+                    text.setTextColor(Color.GREEN);
+                }else {
+                 text.setTextColor(Color.RED);
+                }
+                if(text2.equals(sentence2)){
+                    text2.setTextColor(Color.GREEN);
+                }else {
+                    text2.setTextColor(Color.GREEN);
+                }
+                if(text3.equals(sentence3)){
+                    text3.setTextColor(Color.GREEN);
+                }else {
+                    text3.setTextColor(Color.GREEN);
+                }
+                if(text4.equals(sentence4)){
+                    text4.setTextColor(Color.GREEN);
+                }else {
+                    text4.setTextColor(Color.GREEN);
+                }
+            }
+        });
+
+
+
     }
 }
